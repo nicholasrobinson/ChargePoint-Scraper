12a2f98362a4ae21e78b41f56214213f0f24e881
Nicholas Robinson <nicholasrobinson@macbook.nicholassavilerobinson.com>
2016-10-03 22:22:14 -0700

    Updating to be compatible with latest ChargePoint API

--- chargepoint_scraper.py
+++ chargepoint_scraper.py
@@ -1,6 +1,8 @@
+import copy
 import requests
 import pytz
 import datetime
+import json
 from requests.exceptions import ConnectionError
 
 
@@ -20,41 +22,64 @@ class ChargePointScraper(object):
     LATITUDE_DELTA = 0.00255944575857
     LONGITUDE_DELTA = 0.00730097293854
     AUTH_URL = 'https://na.chargepoint.com/users/validate'
-    STATION_DATA_URL = 'https://na.chargepoint.com/dashboard/getChargeSpots?&lat=%.14f&lng=%.14f&ne_lat=%.14f' + \
-                       '&ne_lng=%.14f&sw_lat=%.14f&sw_lng=%.14f'
+    STATION_DATA_URL = 'https://mc.chargepoint.com/map-prod/get?'
+    STATION_DATA_QUERY_PARAMETERS = {
+        'station_list': {
+            'ne_lat': None,
+            'ne_lon': None,
+            'sw_lat': None,
+            'sw_lon': None,
+            'page_size': 100
+        },
+        'user_id': None
+    }
 
     def __init__(self, username, password, latitude=DEFAULT_LATITUDE, longitude=DEFAULT_LONGITUDE):
         super(ChargePointScraper, self).__init__()
-        self._cookies = None
-        self._username = username
-        self._password = password
-        self._station_data_url = ChargePointScraper.STATION_DATA_URL % (
-            latitude,
-            longitude,
-            latitude + ChargePointScraper.LATITUDE_DELTA,
-            longitude + ChargePointScraper.LONGITUDE_DELTA,
-            latitude - ChargePointScraper.LATITUDE_DELTA,
-            longitude - ChargePointScraper.LONGITUDE_DELTA,
-        )
+        station_data_query_parameters = copy.copy(ChargePointScraper.STATION_DATA_QUERY_PARAMETERS)
+        station_data_query_parameters['station_list']['ne_lat'] = latitude + ChargePointScraper.LATITUDE_DELTA
+        station_data_query_parameters['station_list']['ne_lon'] = longitude + ChargePointScraper.LONGITUDE_DELTA
+        station_data_query_parameters['station_list']['sw_lat'] = latitude - ChargePointScraper.LATITUDE_DELTA
+        station_data_query_parameters['station_list']['sw_lon'] = longitude - ChargePointScraper.LONGITUDE_DELTA
+        station_data_query_parameters['user_id'] = ChargePointScraper._get_user_id(username=username, password=password)
+        self._station_data_url = ChargePointScraper.STATION_DATA_URL + json.dumps(station_data_query_parameters).replace('"', '%22').replace(' ', '')
 
-    def get_station_data(self):
-        if self._cookies is None:
-            self._cookies = self._get_cookies()
+    @staticmethod
+    def _get_user_id(username, password):
         try:
-            r = requests.get(self._station_data_url, cookies=self._cookies)
+            r = requests.post(
+                ChargePointScraper.AUTH_URL,
+                data={
+                    'user_name': username,
+                    'user_password': password
+                }
+            )
+            if r.status_code != requests.codes.ok:
+                raise ChargePointScraperException('Unexpected response status code.')
         except ConnectionError:
-            raise ChargePointScraperException("Unable to connect to server.")
+            raise ChargePointScraperException('Unable to connect to server.')
         try:
             data = r.json()
-            if data[0]['user_info']['is_guest'] == 1:
-                self._clear_cookies()
-                raise ChargePointAuthenticationExpiredException()
-            stations = data[0]['station_list']['summaries']
+            if 'auth' in data and data['auth'] and 'userid':
+                return int(data['userid'])
+            elif 'error' in data:
+                raise ChargePointScraperException('Invalid login. Please try again.')
+        except ValueError:
+            raise ChargePointScraperException('Unable to decode response.')
+
+    def get_station_data(self):
+        try:
+            r = requests.get(self._station_data_url)
+        except ConnectionError:
+            raise ChargePointScraperException('Unable to connect to server.')
+        try:
+            data = r.json()
+            stations = data['station_list']['summaries']
             return {
                 'time': pytz.utc.localize(
-                    datetime.datetime.strptime(data[0]['station_list']['time'], '%Y-%m-%d %H:%M:%S.%f')
+                    datetime.datetime.strptime(data['station_list']['time'], '%Y-%m-%d %H:%M:%S.%f')
                 ).astimezone(
-                    pytz.timezone("America/Los_Angeles")
+                    pytz.timezone('America/Los_Angeles')
                 ),
                 'stations':
                     {
@@ -67,25 +92,6 @@ class ChargePointScraper(object):
                     }
             }
         except ValueError:
-            raise ChargePointScraperException("Unable to decode response.")
+            raise ChargePointScraperException('Unable to decode response.')
         except (IndexError, KeyError):
-            raise ChargePointScraperException("Unexpected response json.")
-
-    def _get_cookies(self):
-        try:
-            r = requests.post(ChargePointScraper.AUTH_URL, data={'user_name': self._username, 'user_password': self._password})
-            if r.status_code != requests.codes.ok:
-                raise ChargePointScraperException("Unexpected response status code.")
-        except ConnectionError:
-            raise ChargePointScraperException("Unable to connect to server.")
-        try:
-            data = r.json()
-            if 'auth' in data and data['auth']:
-                return r.cookies
-            elif 'error' in data:
-                raise ChargePointScraperException('Invalid login. Please try again.')
-        except ValueError:
-            raise ChargePointScraperException("Unable to decode response.")
-
-    def _clear_cookies(self):
-        self._cookies = None
+            raise ChargePointScraperException('Unexpected response json.')
